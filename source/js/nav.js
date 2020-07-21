5eef960eeb02474772f7199c25748368bf7916e0
VyglD <VyglD@yandex.ru>
2020-07-21 09:44:35 +0300

    Refactoring

--- source/js/nav.js
+++ source/js/nav.js
@@ -1,21 +1,17 @@
-// (function () {
-//     var nav = document.querySelector('#nav-js');
-//     var navToggle = document.querySelector('#nav-toggle-js');
+(function () {
+    let nav = document.querySelector('#nav-js');
+    let navToggle = document.querySelector('#nav-toggle-js');
 
-//     var navToggleOpenClass = 'header__nav-toggle--open';
-//     var navOpenClass= 'header__nav--open';
-//     var navCloseClass= 'header__nav--close';
+    nav.classList.remove('header__nav--no-js');
+    navToggle.classList.remove('header__nav-toggle--no-js');
 
-//     nav.classList.remove('header__nav--no-js');
-//     navToggle.classList.remove('header__nav-toggle--no-js');
-
-//     navToggle.addEventListener('click', function(event) {
-//         if(navToggle.classList.contains(navToggleOpenClass)) {
-//             navToggle.classList.remove(navToggleOpenClass);
-//             nav.classList.remove(navOpenClass);
-//         } else {
-//             navToggle.classList.add(navToggleOpenClass);
-//             nav.classList.add(navOpenClass);
-//         }
-//     });
-// })();
+    navToggle.addEventListener('click', function() {
+        if(navToggle.classList.contains('header__nav-toggle--open')) {
+            navToggle.classList.remove('header__nav-toggle--open');
+            nav.classList.remove('header__nav--open');
+        } else {
+            navToggle.classList.add('header__nav-toggle--open');
+            nav.classList.add('header__nav--open');
+        }
+    });
+})();
